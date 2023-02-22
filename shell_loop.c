01d38a482209091bb7ef1cdc9ac1c25497d5c701
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-02-21 20:45:38 -0800

    updates

--- shell_loop.c
+++ shell_loop.c
@@ -6,7 +6,7 @@
  * @av: the argument vector from main()
  *
  * Return: 0 on success, 1 on error, or error code
- */
+*/
 int hsh(info_t *info, char **av)
 {
 	ssize_t r = 0;
@@ -48,15 +48,15 @@ int hsh(info_t *info, char **av)
  * @info: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
- * 	0 if builtin executed successfully,
- * 	1 if builtin found but not successful,
- * 	2 if builtin signals exit()
- */
+*	0 if builtin executed successfully,
+*	1 if builtin found but not successful,
+*	2 if builtin signals exit()
+*/
 int find_builtin(info_t *info)
 {
 	int i, built_in_ret = -1;
 	builtin_table builtintbl[] = {
-		{"exit",_myexit},
+		{"exit", _myexit},
 		{"env", _myenv},
 		{"help", _myhelp},
 		{"history", _myhistory},
